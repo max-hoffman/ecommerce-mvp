1d678607bd9164c5f9c5bfc259a820ba17bab9b5
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-06-12 13:54:59 -0700

    Images endpoint works

---
+++ server/routes/products/images.js
@@ -0,0 +1,19 @@
+const config = require('../../../config/moltin-config');
+const moltin = require('@moltin/sdk');
+const axios = require('axios');
+
+const Moltin = moltin.gateway({
+  client_id: config.CLIENT_ID,
+  client_secret: config.CLIENT_SECRET,
+});
+
+module.exports = {
+  findAllCtrl: function(req, res) {
+    Moltin.Files.All()
+    .then(files => {
+      console.log('files', files);
+      res.status(200).send(files.data);
+    })
+    .catch(error => console.log('moltin fetch error', error));
+  }
+};
--- server/routes/products/index.js
+++ server/routes/products/index.js
@@ -1,12 +1,15 @@
 var router = require('express').Router();
 const products = require('./products');
+const images = require('./images');
 
-router.get('/', products.findAllCtrl);
+router.get('/files', images.findAllCtrl);
 
 router.get('/:id', products.findOneCtrl);
 
 router.put('/:id', products.updateOneCtrl);
 
 router.post('/', products.createOneCtrl);
+
+router.get('/', products.findAllCtrl);
 
 module.exports = router;
